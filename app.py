a588efea2cc44640c4f317bcdfd1e10ecb6cedc3
Sarah Zhou <szhou123@gmail.com>
2020-05-01 16:41:50 -0500

    updated front end

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@127.0.0.1/airbnb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pass@127.0.0.1/airbnb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # mongodb
 app.config['MONGO_URI'] = 'mongodb+srv://user1:user1password@cluster0-9dppt.mongodb.net/crimedata?retryWrites=true&w=majority'
@@ -37,7 +37,6 @@ mysql = MySQL(app)
 mongo = PyMongo(app)
 crimes = mongo.db['crimeRecords']
 CORS(app)
-# Bootstrap(app)
 db2 = SQLAlchemy(app)
 db3 = SQLAlchemy(app)
 #mongo.db.users.insert({'name':name, 'email':email})
@@ -115,7 +114,7 @@ def cal_safety(apartment:list, crimes:list):
             continue
         base_score = crime_score[crime['primary_type']]
         arrest_leverage = 1 if crime['arrest'] else 1.5
-        '''     
+        '''
         if crime['frequency'] > 5:
             fre_leverage = 2
         elif crime['frequency'] <= 2:
@@ -134,6 +133,11 @@ def cal_safety(apartment:list, crimes:list):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# added cafe route
+@app.route('/cafe.html')
+def cafe():
+    return render_template('cafe.html')
 
 @app.route('/database.html')
 def database():
@@ -888,11 +892,6 @@ if __name__ == '__main__':
 
     db3.session.add_all([c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36,c37,c38,c39,c40,c41,c42,c43,c44,c45,c46,c47,c48,c49,c50,c51,c52,c53,c54,c55,c56,c57,c58,c59,c60,c61,c62,c63,c64,c65,c66,c67,c68,c69,c70,c71,c72,c73,c74,c75,c76,c77,c78,c79,c80,c81,c82,c83,c84,c85,c86,c87,c88,c89,c90,c91,c92,c93,c94,c95,c96,c97,c98,c99,c100,c101,c102,c103,c104,c105,c106,c107,c108,c109,c110,c111,c112,c113,c114,c115,c116,c117,c118,c119,c120,c121,c122,c123,c124,c125,c126,c127,c128,c129,c130,c131,c132,c133,c134,c135,c136,c137,c138,c139,c140,c141,c142,c143,c144,c145,c146,c147,c148,c149,c150,c151,c152,c153,c154,c155,c156,c157,c158,c159,c160,c161,c162,c163,c164,c165,c166,c167,c168,c169,c170,c171,c172,c173,c174,c175,c176,c177,c178,c179,c180,c181,c182,c183,c184,c185,c186,c187,c188,c189,c190,c191,c192,c193,c194,c195,c196,c197,c198,c199,c200,c201,c202,c203,c204,c205,c206,c207,c208,c209,c210,c211,c212,c213,c214,c215,c216,c217,c218,c219,c220,c221,c222,c223,c224,c225,c226,c227,c228,c229,c230,c231,c232,c233,c234,c235,c236,c237,c238,c239,c240,c241,c242,c243,c244,c245,c246,c247,c248,c249,c250,c251,c252,c253,c254,c255,c256,c257,c258,c259,c260,c261,c262,c263,c264,c265,c266,c267,c268,c269,c270,c271,c272,c273,c274,c275,c276,c277,c278,c279,c280,c281,c282,c283,c284,c285,c286,c287,c288,c289,c290,c291,c292,c293,c294,c295,c296,c297,c298,c299,c300,c301,c302,c303,c304,c305,c306])
     db3.session.commit()
-    # ap1 = Apartment(ID=1, Description='NICE', Zipcode=61820, NumGuests=3, Price=1000, Landlord='Jack', SafetyRating=5.8)
-    # ap2 = Apartment(ID=2, Description='good', Zipcode=91798, NumGuests=1, Price=200, Landlord='Rose', SafetyRating=4.3)
-    # ap3 = Apartment(ID=3, Description='great', Zipcode=91798, NumGuests=2, Price=500, Landlord='Mike', SafetyRating=5.3)
-
-    # db2.session.add_all([ap1,ap2,ap3])
 
     ap2 = Apartment(ID=1, Description='Modern Boystown/Wrigleyville 1 BDRM', Latitude=41.9488427926778, Longitude=-87.6490462233825, NumGuests=3, Price=103, Landlord='Michael', SafetyRating=0.0)
     ap3 = Apartment(ID=2, Description='The Best Location in Downtown Chi!', Latitude=41.8904217387672, Longitude=-87.6320817379974, NumGuests=2, Price=99, Landlord='Kelsea', SafetyRating=0.0)
@@ -1232,6 +1231,6 @@ if __name__ == '__main__':
         cursor.execute("update apartment set SafetyRating = %s where ID = %s", (rating, apartment[0]))
         db3.commit()
     cursor.close()
-    
+
     """
     app.run(debug=True)
